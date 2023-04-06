83cf9d7c59ac7d0808dce2af8180baac32e421c2
fr-daria <frantsuzova00000@yandex.ru>
2023-04-06 21:32:40 +0300

    Методы без сереализации

--- src/Basket.java
+++ src/Basket.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Arrays;
 
 public class Basket implements Serializable {
 
@@ -24,36 +25,57 @@ public class Basket implements Serializable {
             if (count[i] > 0) {
                 int pprise = prices[i] * count[i];
                 allprise += pprise;
-                System.out.println("Корзина покупок " + productsInBasket[i] + prices[i] + count[i] + pprise);
+                System.out.println("Корзина покупок " + productsInBasket[i] + " " + prices[i] + " " + count[i] + " " + pprise);
             }
             System.out.println("Итого " + allprise);
         }
     }
 
     public void saveTxt(File textFile) { // сохранения корзины в текстовый файл;
-        try (FileOutputStream fos = new FileOutputStream(textFile);
-             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
-            oos.writeObject(this);
-            oos.flush();
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+        try (FileOutputStream fos = new FileOutputStream(textFile)) {
+            for (String product : productsInBasket) {
+                fos.write(product.getBytes());
+            }
+            System.out.println();
+            for (int price : prices) {
+                fos.write(price);
+            }
+            System.out.println();
+            for (int ct : count) {
+                fos.write(ct);
+            }
+            System.out.println();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public static Basket loadFromTxtFile(File textFile) throws IOException, ClassNotFoundException {
-        Basket b1;
-        try (FileInputStream fis = new FileInputStream(textFile);
-             ObjectInputStream ois = new ObjectInputStream(fis)) {
+    public static Basket loadFromTxtFile(File textFile) throws IOException {
+        Basket b  = null;
+        int i;
+        try (BufferedReader ips = new BufferedReader(new FileReader(textFile))) {
+            //int product = fis.read();
+            //b1 = Character.toChars(Integer.parseInt(product, '\n'));
+            //String b2 = String.copyValueOf(b1);
+            String p = ips.readLine();
+            String z = ips.readLine();
+            String c = ips.readLine();
 
-            b1 = (Basket) ois.readObject();
+            b.productsInBasket = p.split(" ");
+            b.prices = Arrays.stream(z.split(" "))
+                    .map(Integer :: parseInt)
+                    .mapToInt(Integer :: intValue)
+                    .toArray();
+            b.count = Arrays.stream(z.split(" "))
+                    .map(Integer :: parseInt)
+                    .mapToInt(Integer :: intValue)
+                    .toArray();
         }
-        return b1;
+        return b;
     }
 
     public void saveBin(File file) throws IOException {
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file))){
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file))) {
             oos.writeObject(this);
             oos.flush();
         }
